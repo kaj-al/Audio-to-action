34788e2e27609ea4604ab009c6b2561d33e56a3f
kaj-al <kt2519033@gmail.com>
2026-02-06 22:01:34 +0530

    to store the tasks

--- pro.py
+++ pro.py
@@ -2,13 +2,15 @@ import streamlit as st
 from pydub import AudioSegment
 import whisper
 import os
-import tempfile
+from datetime import datetime
 from streamlit_mic_recorder import mic_recorder
 
 st.title("AUDIO TO ACTION")
 st.write("Record your voice note and convert it into actions.")
 st.subheader("RECORD VOICE")
 model = whisper.load_model("base")
+if "history" not in st.session_state:
+    st.session_state.history = []
 audio = mic_recorder(start_prompt="Start recording",stop_prompt="Stop recording",just_once=True)
 if audio:
     with open("temp_audio.webm","wb") as f:
@@ -19,5 +21,14 @@ if audio:
         result = model.transcribe("temp_audio.wav")
         st.subheader("recognised text")
         st.write(result["text"])
+
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    st.session_state.history.append(f"{timestamp} - {result["text"]}")
+    with open("transcription.txt","a",encoding="utf-8") as f:
+        f.write(f"{timestamp} - {result["text"]}\n")
+    st.subheader("Voice notes history")
+    for note in reversed(st.session_state.history):
+        st.write(note)
+
 
 
