c91a1cc8ca586b15757e2778c4fc79e22d1d9750
kaj-al <kt2519033@gmail.com>
2026-02-06 20:31:05 +0530

    update  the ui file with whisper model

--- pro.py
+++ pro.py
@@ -1,14 +1,23 @@
 import streamlit as st 
+from pydub import AudioSegment
+import whisper
+import os
+import tempfile
 from streamlit_mic_recorder import mic_recorder
 
 st.title("AUDIO TO ACTION")
 st.write("Record your voice note and convert it into actions.")
 st.subheader("RECORD VOICE")
+model = whisper.load_model("base")
 audio = mic_recorder(start_prompt="Start recording",stop_prompt="Stop recording",just_once=True)
 if audio:
-    st.success("recording successful")
-    st.audio(audio["bytes"],format="audio\wav")
-    with open("recorded.wav","wb") as f:
+    with open("temp_audio.webm","wb") as f:
         f.write(audio["bytes"])
-    st.write("Audio saved")
+        sound = AudioSegment.from_file("temp_audio.webm")
+        sound.export("temp_audio.wav",format="wav")
+
+        result = model.transcribe("temp_audio.wav")
+        st.subheader("recognised text")
+        st.write(result["text"])
+
 
